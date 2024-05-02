8776ee2f494181a75c27b633ebd851d91f2f3b79
ahmad1900z <142015884+ahmad1900z@users.noreply.github.com>
2024-05-02 23:27:27 +0200

    Add files via upload

---
+++ the x design/src/utils/dark-mode.js
@@ -0,0 +1,23 @@
+const darkMode = () => {
+    const themeToggleBtns = document.querySelectorAll('#theme-toggle');
+
+    const theme = localStorage.getItem("theme");
+
+    if (theme) {
+        document.body.classList.add(theme);
+    }
+
+    themeToggleBtns.forEach(btn => {
+        btn.addEventListener('click', () => {
+            document.body.classList.toggle("light-mode");
+            if (document.body.classList.contains("light-mode")) {
+                localStorage.setItem("theme", "light-mode");
+            } else {
+                localStorage.removeItem("theme");
+                document.body.removeAttribute("class");
+            }
+        });
+    });
+};
+
+export default darkMode;
